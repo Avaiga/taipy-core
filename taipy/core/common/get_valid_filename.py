61f0640b50319b45f368aade370310f7eddd2070
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-03-07 09:46:00 +0700

    Update get_valid_filename.py

--- taipy/core/common/get_valid_filename.py
+++ taipy/core/common/get_valid_filename.py
@@ -1,9 +1,17 @@
 import re
 
 
-def get_valid_filename(s):
+class SuspiciousFileOperation(Exception):
+    pass
+
+
+def get_valid_filename(name: str) -> str:
     """
     Source: https://github.com/django/django/blob/main/django/utils/text.py
     """
+    s = str(name).strip().replace(" ", "_")
+    s = re.sub(r"(?u)[^-\w.]", "", s)
+    if s in {"", ".", ".."}:
+        raise SuspiciousFileOperation("Could not derive file name from '%s'" % name)
     s = str(s).strip().replace(" ", "_")
     return re.sub(r"(?u)[^-\w.]", "", s)
