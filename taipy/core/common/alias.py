6d20ee1fd89bb0d63721d0cbb0941a963b2a47ce
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-03-15 14:47:29 +0100

    properly document aliases

--- taipy/core/common/alias.py
+++ taipy/core/common/alias.py
@@ -1,14 +1,14 @@
 from typing import NewType
 
 PipelineId = NewType("PipelineId", str)
-""" Type representing a pipeline identifier."""
+PipelineId.__doc__ = """ Type representing a pipeline identifier."""
 ScenarioId = NewType("ScenarioId", str)
-""" Type representing a scenario identifier."""
+ScenarioId.__doc__ = """ Type representing a scenario identifier."""
 TaskId = NewType("TaskId", str)
-""" Type representing a task identifier."""
+TaskId.__doc__ = """ Type representing a task identifier."""
 JobId = NewType("JobId", str)
-""" Type representing a job identifier."""
+JobId.__doc__ = """ Type representing a job identifier."""
 CycleId = NewType("CycleId", str)
-""" Type representing a cycle identifier."""
+CycleId.__doc__ = """ Type representing a cycle identifier."""
 DataNodeId = NewType("DataNodeId", str)
-""" Type representing a data node identifier."""
+DataNodeId.__doc__ = """ Type representing a data node identifier."""
