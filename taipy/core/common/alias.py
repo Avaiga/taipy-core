70095cda3225e2c7ade53b8ad18bbec8a49ee30b
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-03-15 16:17:29 +0100

    properly document aliases

--- taipy/core/common/alias.py
+++ taipy/core/common/alias.py
@@ -1,14 +1,14 @@
 from typing import NewType
 
 PipelineId = NewType("PipelineId", str)
-PipelineId.__doc__ = """ Type representing a pipeline identifier."""
+PipelineId.__doc__ = """Type that holds a `Pipeline^` identifier."""
 ScenarioId = NewType("ScenarioId", str)
-ScenarioId.__doc__ = """ Type representing a scenario identifier."""
+ScenarioId.__doc__ = """Type that holds a `Scenario^` identifier."""
 TaskId = NewType("TaskId", str)
-TaskId.__doc__ = """ Type representing a task identifier."""
+TaskId.__doc__ = """Type that holds a `Task^` identifier."""
 JobId = NewType("JobId", str)
-JobId.__doc__ = """ Type representing a job identifier."""
+JobId.__doc__ = """Type that holds a `Job^` identifier."""
 CycleId = NewType("CycleId", str)
-CycleId.__doc__ = """ Type representing a cycle identifier."""
+CycleId.__doc__ = """Type that holds a `Cycle^` identifier."""
 DataNodeId = NewType("DataNodeId", str)
-DataNodeId.__doc__ = """ Type representing a data node identifier."""
+DataNodeId.__doc__ = """Type that holds a `DataNode^` identifier."""
