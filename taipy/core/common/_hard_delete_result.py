3ac9333e14688d72d379f45fc907561c4a81eb40
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-03-15 16:49:43 +0700

    Update taipy/core/common/_hard_delete_result.py
    Co-authored-by: Vuillemot Florian <florian.vuillemot@posteo.net>

--- taipy/core/common/_hard_delete_result.py
+++ taipy/core/common/_hard_delete_result.py
@@ -3,7 +3,7 @@ from typing import Set, Union
 
 class _TaskHardDeleteResult:
     def __init__(
-        self, pipeline_data_node_ids: Union[None, Set[str]] = None, scenario_data_node_ids: Union[None, Set[str]] = None
+        self, pipeline_data_node_ids: Set[str] = None, scenario_data_node_ids: Set[str] = None
     ):
         self.pipeline_data_node_ids = pipeline_data_node_ids if pipeline_data_node_ids is not None else set()
         self.scenario_data_node_ids = scenario_data_node_ids if scenario_data_node_ids is not None else set()
