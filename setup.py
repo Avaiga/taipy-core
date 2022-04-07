f7b65eeca1ecfe55dbcae2d3821ec35d02338fea
Vuillemot Florian <florian.vuillemot@posteo.net>
2022-04-07 14:55:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/avaiga/taipy-core",
-    version="1.0.0.dev3",
+    version="1.0.0",
     zip_safe=False,
     extras_require=extras_require,
 )
