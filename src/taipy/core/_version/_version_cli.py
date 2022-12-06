a8e9a89a2389d62946b62551d1372c9db32f47c7
trgiangdo <dtr.giang.1299@gmail.com>
2022-12-06 12:30:47 +0700

    refactor versioning cli

--- src/taipy/core/_version/_version_cli.py
+++ src/taipy/core/_version/_version_cli.py
@@ -9,19 +9,8 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import uuid
 
 import click
-
-from taipy.logger._taipy_logger import _TaipyLogger
-
-from ..cycle._cycle_manager_factory import _CycleManagerFactory
-from ..data._data_manager_factory import _DataManagerFactory
-from ..job._job_manager_factory import _JobManagerFactory
-from ..pipeline._pipeline_manager_factory import _PipelineManagerFactory
-from ..scenario._scenario_manager_factory import _ScenarioManagerFactory
-from ..task._task_manager_factory import _TaskManagerFactory
-from ._version_manager import _VersionManager
 
 
 def skip_run(ctx, param, value):
@@ -32,39 +21,42 @@ def skip_run(ctx, param, value):
     ctx.exit()
 
 
-def clean_all_entities_by_version(version_number):
-    """Delete all entities belongs to a version from the Taipy data folder."""
-    _JobManagerFactory._build_manager()._delete_by_version(version_number)
-    _ScenarioManagerFactory._build_manager()._delete_by_version(version_number)
-    _PipelineManagerFactory._build_manager()._delete_by_version(version_number)
-    _TaskManagerFactory._build_manager()._delete_by_version(version_number)
-    _DataManagerFactory._build_manager()._delete_by_version(version_number)
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
-@click.command()
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
     "--development",
+    "--dev",
     "-d",
     "mode",
     flag_value="development",
     default=True,
-    help="Execute Taipy application in DEVELOPMENT mode. This is the default behavior.",
+    help="""
+        When execute Taipy application in `development` mode, all entities from the previous development version will
+        be deleted before running new Taipy application.
+        This is the default behavior.
+    """,
 )
 @click.option(
-    "--experiment", "-e", "mode", flag_value="experiment", help="Execute Taipy application in EXPERIMENTAL mode."
+    "--experiment",
+    "-e",
+    "mode",
+    flag_value="experiment",
+    help="""
+        When execute Taipy application in `experiment` mode, the current Taipy application is saved to a new version
+        defined by "--version-number".
+    """,
 )
 @click.option(
     "--version-number",
-    "-v",
-    "_version_number",
     type=str,
     default=None,
-    help="The version number when execute and save in experimental mode. If not provided, a random version name is used.",
+    help="The version number when execute in `experiment` mode. If not provided, a random version name is used.",
 )
 @click.option(
     "--override",
     "-o",
-    "_override",
     is_flag=True,
     help='Override the version specified by "--version-number" if existed. Default to False.',
 )
@@ -75,27 +67,7 @@ def clean_all_entities_by_version(version_number):
     callback=skip_run,
     expose_value=False,
     is_eager=True,
-    help='Save the "latest" version if existed to a new version specified by "--version-number" without running the application. Default to False.',
+    help='Save the development version if existed to a new version specified by "--version-number" without running the application. Default to False.',
 )
-def version_cli(mode, _version_number, _override):
-    if mode == "experiment":
-        if _version_number:
-            curren_version_number = _version_number
-        else:
-            curren_version_number = str(uuid.uuid4())
-
-        override = _override
-
-    elif mode == "development":
-        curren_version_number = _VersionManager.get_development_version()
-        _VersionManager.set_development_version(curren_version_number)
-        override = True
-
-        clean_all_entities_by_version(curren_version_number)
-        _TaipyLogger._get_logger().info(f"Development mode: Clean all entities with version {curren_version_number}")
-
-    else:
-        _TaipyLogger._get_logger().error("Undefined execution mode.")
-        return
-
-    _VersionManager.set_current_version(curren_version_number, override)
+def version_cli(mode, version_number, override):
+    return mode, version_number, override
